34ca261cf18c81a7cad1c377b81d323902f53212
Yugene42 <admin@wfw.ru>
2023-11-08 18:51:52 +0300

    Test

--- src/test/java/steps/BaseSteps.java
+++ src/test/java/steps/BaseSteps.java
@@ -15,7 +15,7 @@ public class BaseSteps {
     public void setupDriver() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver/chromedriver");
         ChromeOptions options = new ChromeOptions();
-        System.setProperty("webdriver.chrome.whitelistedIps", "");
+//        System.setProperty("webdriver.chrome.whitelistedIps", "");
         options.setCapability("acceptInsecureCerts", true);
         options.setCapability("pageLoadStrategy", "eager");
         options.addArguments("--ignore-certificate-errors");
@@ -23,12 +23,12 @@ public class BaseSteps {
 //        options.addArguments("window-size="+Properties.getBrowserSize());
 
 
-        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+//        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 //        options.addArguments("--headless"); // open Browser headless
 //        options.addArguments("--disable-infobars"); // disabling infobars
 //        options.addArguments("--disable-extensions"); // disabling extensions
 //        options.addArguments("--disable-gpu"); // applicable to windows os only
-        options.addArguments("--no-sandbox"); // Bypass OS security model
+//        options.addArguments("--no-sandbox"); // Bypass OS security model
 
         driver = new ChromeDriver(options); // присваиваем значение driver
     }
