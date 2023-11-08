ba888340c12df59f1c5ab5e52f314649ef80a38b
Yugene42 <admin@wfw.ru>
2023-11-08 18:19:04 +0300

    Test

--- src/test/java/steps/BaseSteps.java
+++ src/test/java/steps/BaseSteps.java
@@ -25,9 +25,9 @@ public class BaseSteps {
 
 
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-////        options.addArguments("start-maximized"); // open Browser in maximized mode
+//        options.addArguments("start-maximized"); // open Browser in maximized mode
         options.addArguments("disable-infobars"); // disabling infobars
-//        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-extensions"); // disabling extensions
 //        options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--no-sandbox"); // Bypass OS security model
 
