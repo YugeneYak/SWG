eb91b9f38ac2627f3921347629d064fd61f1a844
Yugene42 <admin@wfw.ru>
2023-11-08 18:00:27 +0300

    Test

--- src/test/java/steps/BaseSteps.java
+++ src/test/java/steps/BaseSteps.java
@@ -24,11 +24,11 @@ public class BaseSteps {
         options.addArguments("window-size="+Properties.getBrowserSize());
 
 
-        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-//        options.addArguments("start-maximized"); // open Browser in maximized mode
-        options.addArguments("disable-infobars"); // disabling infobars
-        options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
+//        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+////        options.addArguments("start-maximized"); // open Browser in maximized mode
+//        options.addArguments("disable-infobars"); // disabling infobars
+//        options.addArguments("--disable-extensions"); // disabling extensions
+//        options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--no-sandbox"); // Bypass OS security model
 
         driver = new ChromeDriver(options); // присваиваем значение driver
