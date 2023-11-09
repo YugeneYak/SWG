507c18565e07a94f423e88b27c2c55c228b1a5ec
Yugene42 <admin@wfw.ru>
2023-11-09 16:26:58 +0300

    Test

--- src/test/java/steps/BaseSteps.java
+++ src/test/java/steps/BaseSteps.java
@@ -28,25 +28,10 @@ public class BaseSteps {
     // задаем параметры открытия браузера
     public void setupDriver() {
 
-//        try {
-//            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver/chromedriver");
-//            ChromeOptions chromeOptions = new ChromeOptions();
-//            chromeOptions.addArguments("--headless");
-//            chromeOptions.addArguments("--no-sandbox");
-//            chromeOptions.addArguments("--disable-dev-shm-usage");
-//            WebDriver d = new ChromeDriver(chromeOptions);
-//            d.get("https://www.google.nl/");
-//        }catch (RuntimeException e) {
-//            System.out.println("ошибки=" + e.toString() + "=ошибки");
-//            System.out.println("trass=====================");
-//            e.printStackTrace();
-//            System.out.println("====================trass");
-//        }
-
         System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/chromedriver/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
-        options.addArguments("--no-sandbox"); // Bypass OS security model
+//        options.addArguments("--no-sandbox"); // Bypass OS security model
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--disable-extensions"); // disabling extensions
         options.addArguments("--disable-gpu"); // applicable to windows os only
