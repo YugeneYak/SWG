b5bb372c8147b4d7f18d7dad3da5a0e2b85b46c1
Yugene42 <admin@wfw.ru>
2023-11-09 08:20:13 +0300

    Test

--- src/test/java/steps/BaseSteps.java
+++ src/test/java/steps/BaseSteps.java
@@ -27,7 +27,7 @@ public class BaseSteps {
         options.addArguments("--headless"); // open Browser headless
         options.addArguments("--disable-infobars"); // disabling infobars
         options.addArguments("--disable-extensions"); // disabling extensions
-//        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--no-sandbox"); // Bypass OS security model
 
         driver = new ChromeDriver(options); // присваиваем значение driver
