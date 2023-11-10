233a78be469f334693563e94db5009f3bebd7a8d
Yugene42 <admin@wfw.ru>
2023-11-10 12:54:13 +0300

    Test

--- src/test/java/steps/BaseSteps.java
+++ src/test/java/steps/BaseSteps.java
@@ -35,22 +35,21 @@ public class BaseSteps {
         ChromeOptions options = new ChromeOptions();
         options.setBinary("/chrome/linux-119.0.6045.105/chrome-linux64/chrome");
         options.addArguments("--no-sandbox"); // Bypass OS security model
-        options.addArguments("--headless");
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-//        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--headless");
+        options.addArguments("--disable-extensions"); // disabling extensions
 //        options.addArguments("--disable-gpu"); // applicable to windows os only
 ////        System.setProperty("webdriver.chrome.whitelistedIps", "");
-////        options.setCapability("acceptInsecureCerts", true);
-////        options.setCapability("pageLoadStrategy", "eager");
-////        options.addArguments("--ignore-certificate-errors");
+        options.setCapability("acceptInsecureCerts", true);
+        options.setCapability("pageLoadStrategy", "eager");
+        options.addArguments("--ignore-certificate-errors");
 ////        options.addArguments("--allowed-ips=''");
-////        options.addArguments("window-size="+Properties.getBrowserSize());
+        options.addArguments("window-size="+Properties.getBrowserSize());
 
-////        options.addArguments("--remote-debugging-pipe");
-////        options.addArguments("--enable-logging --v=1"); // Bypass OS security model
-////
-////        options.addArguments("--headless"); // open Browser headless
-////        options.addArguments("--disable-infobars"); // disabling infobars
+        options.addArguments("--remote-debugging-pipe");
+        options.addArguments("--enable-logging --v=1"); // Bypass OS security model
+
+        options.addArguments("--disable-infobars"); // disabling infobars
 ////        options.addArguments("--disable-gpu"); // applicable to windows os only
 
 //        ChromeOptions options = new ChromeOptions();
