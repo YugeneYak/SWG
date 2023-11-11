247da8f0c207422e44da837fe0bef284e49a3671
Yugene42 <admin@wfw.ru>
2023-11-11 09:33:50 +0300

    Test

--- src/test/java/steps/TestsSteps.java
+++ src/test/java/steps/TestsSteps.java
@@ -55,7 +55,7 @@ public class TestsSteps {
 
 //        Assert.assertTrue(driver.findElement(byCssSelector(".cookie_button")).isDisplayed());
         driver.findElement(byCssSelector(".cookie_button")).click(); //кликаем кнопку
-
+        driver.quit();
     }
 
     @Тогда("Открываем меню каталога")
