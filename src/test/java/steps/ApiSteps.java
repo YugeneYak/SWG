112495b76465ee5b67fb4399da9e619ab052c96c
Yugene42 <admin@wfw.ru>
2023-11-16 10:13:45 +0300

    Добавлены тесты API

--- src/test/java/steps/ApiSteps.java
+++ src/test/java/steps/ApiSteps.java
@@ -37,7 +37,7 @@ public class ApiSteps {
         // Создание экземпляра класса BaseSteps
         BaseSteps baseSteps = new BaseSteps();
         // Инициализация драйвера
-//        baseSteps.setupDriver();
+        baseSteps.setupDriver();
         // Задание значения драйвера
         driver = baseSteps.getDriver();
     }
